3a5db8f2ba15a996c7625888f7787474a0e24781
FabianCastillo12 <132018077+FabianCastillo12@users.noreply.github.com>
2024-09-10 17:14:51 -0500

    Update update-cliente.dto.ts

--- src/clientes/dto/update-cliente.dto.ts
+++ src/clientes/dto/update-cliente.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClienteDto } from './create-cliente.dto';
+import { IsEmail, IsString, IsOptional } from "class-validator";
 
-export class UpdateClienteDto extends PartialType(CreateClienteDto) {}
+export class UpdateClienteDto extends PartialType(CreateClienteDto) {
+    @IsString()
+    @IsOptional()
+    nombre:string;
+    @IsEmail()
+    @IsOptional()
+    email:string;
+    @IsString()
+    @IsOptional()
+    telefono:string;
+    @IsString()
+    @IsOptional()
+    direccion:string;
+    @IsString()
+    @IsOptional()
+    dni:string;
+    @IsString()
+    @IsOptional()
+    ruc:string;
+}
